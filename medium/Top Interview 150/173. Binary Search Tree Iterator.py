5e89906af303fbd4c4462eaee1a800d7b973832d
pOsdas <ratmir1099@mail.ru>
2025-01-23 03:00:05 +0300

    173. Binary Search Tree Iterator

---
+++ medium/Top Interview 150/173. Binary Search Tree Iterator.py
@@ -0,0 +1,24 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class BSTIterator:
+    def __init__(self, root: TreeNode):
+        self.stack = []
+        self._leftmost_inorder(root)
+
+    def _leftmost_inorder(self, node):
+        while node:
+            self.stack.append(node)
+            node = node.left
+
+    def next(self) -> int:
+        top_node = self.stack.pop()
+        if top_node.right:
+            self._leftmost_inorder(top_node.right)
+        return top_node.val
+
+    def hasNext(self) -> bool:
+        return len(self.stack) > 0
